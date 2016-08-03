eac388249da149c47ea185105c7b7d87c8ec729e
achensetyawati <achenzetya@gmail.com>
2016-08-03 18:11:05 +0700

    add router buyer

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ server.use(restify.bodyParser());
 server.use(restify.CORS());
 
 var v1ProductRouter = require('./src/routers/v1/core/product-router');
+var v2BuyerRouter=require('./src/routers/v1/core/buyer-router');
 v1ProductRouter.applyRoutes(server); 
+v2BuyerRouter.applyRoutes(server); 
 
 server.listen(process.env.PORT, process.env.IP);
 console.log(`server created at ${process.env.IP}:${process.env.PORT}`)
