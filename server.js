1dfb485d45b21e79c7efff9462d75f870fd36f2a
Tris Setiawan <tris.setiawan@yahoo.com>
2016-08-03 09:53:31 +0000

    1.0.0 - add product router

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var restify = require('restify');
+var server = restify.createServer();
+
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(restify.CORS());
+
+var v1ProductRouter = require('./src/routers/v1/core/product-router');
+v1ProductRouter.applyRoutes(server); 
+
+server.listen(process.env.PORT, process.env.IP);
+console.log(`server created at ${process.env.IP}:${process.env.PORT}`)
