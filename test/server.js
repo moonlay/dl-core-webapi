bb3d121763b090a92ee920bd39c2c159c6e4fd8e
Indri.Hutabalian <indri.hutabalian@moonlay.com>
2016-08-24 18:12:20 +0700

    add po router

--- test/server.js
+++ test/server.js
@@ -43,7 +43,10 @@ module.exports = new Promise((resolve, reject) => {
 
         var v1POGarmentGeneralRouter = require('../src/routers/v1/po/po-garment-fabric-router');
         v1POGarmentGeneralRouter.applyRoutes(server);
-
+        
+        var v1POTextileJobOrderRouter = require('../src/routers/v1/po/po-textile-general-atk-router');
+        v1POTextileJobOrderRouter.applyRoutes(server);
+        
         server.listen(process.env.PORT, process.env.IP);
         console.log(`server created at ${process.env.IP}:${process.env.PORT}`);
         resolve(`${process.env.IP}:${process.env.PORT}`);
