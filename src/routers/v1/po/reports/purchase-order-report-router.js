fc22b8bed59a99d60101dbb5fd67fbe661856fe5
achensetyawati <achenzetya@gmail.com>
2016-10-01 08:37:10 +0700

    adding router for report category

--- src/routers/v1/po/reports/purchase-order-report-router.js
+++ src/routers/v1/po/reports/purchase-order-report-router.js
@@ -28,4 +28,49 @@ router.get("/v1/po/poreport?dateFrom=:sdate&dateTo=:edate", function(request, re
         })
 });
 
+router.get("/v1/po/poreport/:unit?dateFrom=:sdate&dateTo=:edate", function(request, response, next) {
+    db.get().then(db => {
+            var manager = new PurchaseOrderManager(db, {
+                username: 'router'
+            });
+            var sdate = request.params.dateFrom;
+            var edate = request.params.dateTo;
+            var unit = request.params.unit;
+            manager.getDataPODetailUnit(sdate,edate,unit)
+            .then(docs => {
+                    var result = resultFormatter.ok(apiVersion, 200, docs);
+                    response.send(200, result);
+                })
+                .catch(e => {
+                    response.send(500, "gagal ambil data");
+                })
+        })
+        .catch(e => {
+            var error = resultFormatter.fail(apiVersion, 400, e);
+            response.send(400, error);
+        })
+});
+
+router.get("/v1/po/poreportcategory?dateFrom=:sdate&dateTo=:edate", function(request, response, next) {
+    db.get().then(db => {
+            var manager = new PurchaseOrderManager(db, {
+                username: 'router'
+            });
+            var sdate = request.params.dateFrom;
+            var edate = request.params.dateTo;
+            manager.getDataPOCategory(sdate,edate)
+            .then(docs => {
+                    var result = resultFormatter.ok(apiVersion, 200, docs);
+                    response.send(200, result);
+                })
+                .catch(e => {
+                    response.send(500, "gagal ambil data");
+                })
+        })
+        .catch(e => {
+            var error = resultFormatter.fail(apiVersion, 400, e);
+            response.send(400, error);
+        })
+});
+
 module.exports = router;
