1afd7fc2b57375560b2caa4c7a81c8816fe11f92
achensetyawati <achenzetya@gmail.com>
2016-10-13 11:43:27 +0700

    updating router and URI for reports

--- src/routers/v1/purchasing/reports/purchase-order-report-unit-router.js
+++ src/routers/v1/purchasing/reports/purchase-order-report-unit-router.js
@@ -6,14 +6,11 @@ const apiVersion = '1.0.0';
 var PurchaseOrderManager = require("dl-module").managers.purchasing.PurchaseOrderManager;
 var passport = require('../../../../passports/jwt-passport');
 
-router.get("/:unit",passport, function(request, response, next) {
+router.get("/",passport, function(request, response, next) {
     db.get().then(db => {
             var manager = new PurchaseOrderManager(db, request.user);
             var sdate = request.params.dateFrom;
             var edate = request.params.dateTo;
-            var unit=request.params.unit;
-            if(unit=="undefined")
-            {
                 manager.getDataPOUnit(sdate, edate)
                     .then(docs => {
                         if ((request.headers.accept || '').toString().indexOf("application/xls") < 0) {
@@ -27,22 +24,33 @@ router.get("/:unit",passport, function(request, response, next) {
                     .catch(e => {
                         response.send(500, "gagal ambil data");
                     })
+            
+        }).catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+});
+
+router.get("/:unit",passport, function(request, response, next) {
+    db.get().then(db => {
+            var manager = new PurchaseOrderManager(db, request.user);
+            var sdate = request.params.dateFrom;
+            var edate = request.params.dateTo;
+            var unit=request.params.unit;
+            
+        manager.getDataPODetailUnit(sdate, edate, unit)
+        .then(docs => {
+            if ((request.headers.accept || '').toString().indexOf("application/xls") < 0) {
+                var result = resultFormatter.ok(apiVersion, 200, docs);
+                response.send(200, result);
             }
-            else
-            {
-                manager.getDataPODetailUnit(sdate, edate, unit)
-                .then(docs => {
-                    if ((request.headers.accept || '').toString().indexOf("application/xls") < 0) {
-                        var result = resultFormatter.ok(apiVersion, 200, docs);
-                        response.send(200, result);
-                    }
-                    else {
-                        response.xls('data.xlsx', docs);
-                    }
-                }).catch(e => {
-                            response.send(500, "gagal ambil data");
-                        })
+            else {
+                response.xls('data.xlsx', docs);
             }
+        }).catch(e => {
+                    response.send(500, "gagal ambil data");
+                })
+            
         }).catch(e => {
                 var error = resultFormatter.fail(apiVersion, 400, e);
                 response.send(400, error);
