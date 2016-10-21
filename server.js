8cd5fb05fd043d4133f864be56ec53837ddea231
indriHutabalian <indri.hutabalian@moonlay.com>
2016-10-21 09:45:47 +0700

    add unit payment price correction (#65)
    * add unit payment price correction note
    
    * router for unit-payment-order-router
    
    * add router unitPaymentOrder

--- server.js
+++ server.js
@@ -101,6 +101,12 @@ v1UnitReceiptNoteMonitoring.applyRoutes(server, "/v1/purchasing/receipt-note/uni
 var v1UnitReceiptNote = require('./src/routers/v1/purchasing/unit-receipt-note-router');
 v1UnitReceiptNote.applyRoutes(server, "/v1/purchasing/receipt-note/unit");
 
+var v1UnitPaymentPriceCorrectionNote = require('./src/routers/v1/purchasing/unit-payment-price-correction-note-router');
+v1UnitPaymentPriceCorrectionNote.applyRoutes(server, "/v1/purchasing/unit-payment-note/price-correction");
+
+var v1UnitPaymentOrder = require('./src/routers/v1/purchasing/unit-payment-order-router');
+v1UnitPaymentOrder.applyRoutes(server, "/v1/purchasing/payment-order/unit");
+
 // var v1UnitReceiptNote = require('./src/routers/v1/purchasing/unit-receipt-note-pdf-router');
 // v1UnitReceiptNote.applyRoutes(server, "/v1/purchasing/receipt-note/unit/pdf"); 
 
