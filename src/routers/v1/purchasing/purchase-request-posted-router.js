6f1df11e933fb617b7743e243ece1173ab3f4b85
achensetyawati <achenzetya@gmail.com>
2016-11-02 08:46:10 +0700

    adding PR posted router

---
+++ src/routers/v1/purchasing/purchase-request-posted-router.js
@@ -0,0 +1,40 @@
+var Router = require('restify-router').Router;
+var router = new Router();
+var db = require("../../../db");
+var PurchaseRequestManager = require("dl-module").managers.purchasing.PurchaseRequestManager;
+var resultFormatter = require("../../../result-formatter");
+
+var passport = require('../../../passports/jwt-passport');
+const apiVersion = '1.0.0';
+
+
+router.get("/", passport, (request, response, next) => {
+    db.get().then(db => {
+        var manager = new PurchaseRequestManager(db, request.user);
+
+        var query = request.queryInfo;
+
+        var filter = {
+            isPosted: true
+        };
+
+        query.filter = filter;
+
+        manager.read(query)
+            .then(docs => {
+                var result = resultFormatter.ok(apiVersion, 200, docs.data);
+                delete docs.data;
+                result.info = docs;
+                response.send(200, result);
+            })
+            .catch(e => {
+                response.send(500, "gagal ambil data");
+            });
+    })
+        .catch(e => {
+            var error = resultFormatter.fail(apiVersion, 400, e);
+            response.send(400, error);
+        });
+});
+
+module.exports = router;
