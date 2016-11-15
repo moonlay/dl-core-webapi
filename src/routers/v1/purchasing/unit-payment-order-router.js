c1ec7461274fff06e5396d7b41afc350fa327841
Indri.Hutabalian <indri.hutabalian@moonlay.com>
2016-11-15 09:44:19 +0700

    fix query select (add division.name)

--- src/routers/v1/purchasing/unit-payment-order-router.js
+++ src/routers/v1/purchasing/unit-payment-order-router.js
@@ -17,8 +17,8 @@ router.get("/", passport, (request, response, next) => {
         };
         var query = request.queryInfo;
         query.order = sorting;
-        query.select=[
-            "unit.division.name","unit.name","supplier.name","date","no","items"
+        query.select = [
+            "division.name", "supplier.name", "date", "no", "items"
         ];
         manager.read(query)
             .then(docs => {
