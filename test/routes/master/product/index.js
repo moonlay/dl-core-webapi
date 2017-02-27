2b0efe01ef327e68bc84d0673db145199e1102e1
indriHutabalian <indri.hutabalian@moonlay.com>
2017-02-27 17:07:02 +0700

    add unit test by id

--- test/routes/master/product/index.js
+++ test/routes/master/product/index.js
@@ -1,11 +1,15 @@
-describe("BASIC SCENARIOS", function() {
-    require("./basic");
-});
+// describe("BASIC SCENARIOS", function() {
+//     require("./basic");
+// });
 
-describe("UNAUTHORIZED SCENARIOS", function() {
-    require("./unauthorized");
-});
+// describe("UNAUTHORIZED SCENARIOS", function() {
+//     require("./unauthorized");
+// });
 
-describe("UPLOAD SCENARIOS", function() {
-    require("./upload");
+// describe("UPLOAD SCENARIOS", function() {
+//     require("./upload");
+// });
+
+describe("READ SCENARIOS", function() {
+    require("./read-by-id");
 });
