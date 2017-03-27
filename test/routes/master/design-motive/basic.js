5f8de47a9c8e451077f18ba60f362f9e54e19e75
achensetyawati <achenzetya@gmail.com>
2017-03-27 10:13:22 +0700

    fix design-motives test

--- test/routes/master/design-motive/basic.js
+++ test/routes/master/design-motive/basic.js
@@ -1,6 +1,6 @@
  var basicTest = require("../../basic-test-factory");
  basicTest({
-     uri: "/master/design-motivess",
+     uri: "/master/design-motives",
      model: require("dl-models").master.DesignMotive,
      validate: require("dl-models").validator.master.designMotive,
      util: require("dl-module").test.data.master.designMotive,
