fba2fe1b9359542f1eb7d80f71dd46d8b411a899
indriHutabalian <indri.hutabalian@moonlay.com>
2017-07-10 17:33:17 +0700

    add router garment product

---
+++ test/routes/master/garment-product/index.js
@@ -0,0 +1,15 @@
+describe("BASIC SCENARIOS", function() {
+    require("./basic");
+});
+
+describe("UNAUTHORIZED SCENARIOS", function() {
+    require("./unauthorized");
+});
+
+describe("UPLOAD SCENARIOS", function() {
+    require("./upload");
+});
+
+describe("READ SCENARIOS", function() {
+    require("./read-by-id");
+});
