352f8aced36986dd48175beaacf56bef1d92b848
indriHutabalian <indri.hutabalian@moonlay.com>
2017-08-04 17:05:01 +0700

    add fp-duration-estimation-router

---
+++ test/routes/master/fp-duration-estimation/basic.js
@@ -0,0 +1,8 @@
+ var basicTest = require("../../basic-test-factory");
+ basicTest({
+     uri: "/master/fp-duration-estimations",
+     model: require("dl-models").master.FinishingPrintingDurationEstimation,
+     validate: require("dl-models").validator.master.finishingPrintingDurationEstimation,
+     util: require("dl-module").test.data.master.fpDurationEstimation,
+     keyword: "name"
+ });
