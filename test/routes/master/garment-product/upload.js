e1189b09273d4ccd21899feb04b5c7c755daf30d
leslie.aula <leslie.aula@moonlay.com>
2018-06-21 16:02:09 +0700

    Update Unit Test

--- test/routes/master/garment-product/upload.js
+++ test/routes/master/garment-product/upload.js
@@ -7,8 +7,8 @@ var ObjectId = require("mongodb").ObjectId;
 var request = Request(host);
 var jwt;
 
-before("#00. init server", function(done){
-    server().then(server=>{
+before("#00. init server", function (done) {
+    server().then(server => {
         request = Request(server);
         done();
     });
@@ -16,15 +16,15 @@ before("#00. init server", function(done){
 
 it(`#01. should error when upload invalid file`, function (done) {
     request
-        .post('/v1/master/upload-garment-products') 
+        .post('/v1/master/upload-garment-products')
         .attach('fileUpload', "test/files/Master Garment Barang - Invalid.csv")
         .end(function (err, response) {
             if (err)
                 done(err);
             else {
-                var result= response.header;
+                var result = response.header;
                 result.should.have.property("content-type");
-                result.should.have.property("date"); 
+                result.should.have.property("date");
                 done();
             }
         });
@@ -32,12 +32,32 @@ it(`#01. should error when upload invalid file`, function (done) {
 
 it(`#02. should succes when upload valid file`, function (done) {
     request
-        .post('/v1/master/upload-garment-products') 
+        .post('/v1/master/upload-garment-products')
         .attach('fileUpload', "test/files/Master Garment Barang - Valid.csv")
         .end(function (err, response) {
             if (err)
                 done(err);
-            else { 
+            else {
+                done();
+            }
+        });
+});
+
+it(`#03. should succes when get distinct data`, function (done) {
+    request
+        .get('/v1/master/read/distinct-product-description')
+        .set("authorization", `Bearer ${jwt}`)
+        .set("Accept", "application/json")
+        .expect(200)
+        .expect("Content-Type", "application/json")
+        .end(function (err, response) {
+            if (err)
+                done(err);
+            else {
+                var result = response.body;
+                result.should.have.property("apiVersion");
+                result.should.have.property("data");
+                result.data.should.instanceOf(Array);
                 done();
             }
         });
