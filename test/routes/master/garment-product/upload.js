424f28a17596a9e88cc286af37ec6f3997505bdd
leslie.aula <leslie.aula@moonlay.com>
2018-06-21 17:20:05 +0700

    Update Unit Test

--- test/routes/master/garment-product/upload.js
+++ test/routes/master/garment-product/upload.js
@@ -22,34 +22,34 @@ before("#00. init server", function (done) {
     });
 })
 
-// it(`#01. should error when upload invalid file`, function (done) {
-//     request
-//         .post('/v1/master/upload-garment-products')
-//         .attach('fileUpload', "test/files/Master Garment Barang - Invalid.csv")
-//         .end(function (err, response) {
-//             if (err)
-//                 done(err);
-//             else {
-//                 var result = response.header;
-//                 result.should.have.property("content-type");
-//                 result.should.have.property("date");
-//                 done();
-//             }
-//         });
-// });
+it(`#01. should error when upload invalid file`, function (done) {
+    request
+        .post('/v1/master/upload-garment-products')
+        .attach('fileUpload', "test/files/Master Garment Barang - Invalid.csv")
+        .end(function (err, response) {
+            if (err)
+                done(err);
+            else {
+                var result = response.header;
+                result.should.have.property("content-type");
+                result.should.have.property("date");
+                done();
+            }
+        });
+});
 
-// it(`#02. should succes when upload valid file`, function (done) {
-//     request
-//         .post('/v1/master/upload-garment-products')
-//         .attach('fileUpload', "test/files/Master Garment Barang - Valid.csv")
-//         .end(function (err, response) {
-//             if (err)
-//                 done(err);
-//             else {
-//                 done();
-//             }
-//         });
-// });
+it(`#02. should succes when upload valid file`, function (done) {
+    request
+        .post('/v1/master/upload-garment-products')
+        .attach('fileUpload', "test/files/Master Garment Barang - Valid.csv")
+        .end(function (err, response) {
+            if (err)
+                done(err);
+            else {
+                done();
+            }
+        });
+});
 
 it(`#03. should succes when get distinct data`, function (done) {
     request
